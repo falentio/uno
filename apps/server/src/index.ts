f6634b43690d422def1987c13b198c4fdc0872a9
falentio <riv.kevinfalent.io@gmail.com>
2023-03-11 12:05:59 +0700

    add server apps

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,15 +1,50 @@
-import * as express from "express"
+import express from "express"
 import * as http from "http"
-import * as ws from "ws"
+import { Server } from "socket.io"
+import { Game, Player } from "@uno/core"
 
 export function createServer() {
 	const app = express()
 	const server = http.createServer(app)
-	const wss = new ws.WebSocket({ server })
+	const io = new Server(server)
+	const players = new Set<string>()
+	let games = [] as Game[]
+
+	io.on("connection", socket => {
+		const url = new URL(socket.request.url || "", "https://uno/")
+		const name = url.searchParams.get("name") + "::" + performance.now().toString(36)
+	})
+
+	// wss.on("connection", (ws, req) => {
+	// 	const url = new URL(req.url, "https://uno/")
+	// 	const name = url.searchParams.get("name") + "::" + performance.now().toString(36)
+	// 	players.add(name)
+	// 	ws.on("message", data => {
+	// 		try {
+	// 			const { event, game, data } = JSON.parse(data)
+	// 			switch (event) {
+	// 				case "join": {
+	// 					const g = games.find(g => g.id === "game")
+	// 					g.join()
+	// 				}
+	// 			}
+	// 		} catch (e) {
+	// 			console.error(e)
+	// 		}
+	// 	})
+	// 	ws.on("close", () => {
+	// 		players.remove(name)
+	// 		for (const g of games) {
+	// 			g.leave(name)
+	// 		}
+	// 	})
+	// })
 
 	return {
 		app,
 		server,
-		wss,
+		io,
+		players,
+		games,
 	}
 }
